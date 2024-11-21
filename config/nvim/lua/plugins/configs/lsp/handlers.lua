546f016302e1e5857876e1f8503ca21ee92d6643
Kaelon Swanton <kaelonswanton@gmail.com>
2024-11-20 17:56:17 -0800

    Fixes rust issues

--- config/nvim/lua/plugins/configs/lsp/handlers.lua
+++ config/nvim/lua/plugins/configs/lsp/handlers.lua
@@ -61,6 +61,16 @@ M.setup = function()
       update_in_insert = false
     }
   )
+
+  for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+  end
 end
 
 -- Provides navigation for LSP symbols for barbecue.nvim
