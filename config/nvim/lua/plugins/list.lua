6f904000356a57ad533ba5a2400a0b8c87c5fb95
Kaelon Swanton <kaelonswanton@gmail.com>
2024-12-03 23:46:36 -0800

    Removes double enabled from indent-blankline

--- config/nvim/lua/plugins/list.lua
+++ config/nvim/lua/plugins/list.lua
@@ -174,7 +174,6 @@ local events = {
   {
     -- Indentations on left side of lines
     "lukas-reineke/indent-blankline.nvim",
-    enabled = true,
     config = require("plugins.configs.indent-blankline"),
     enabled = false,
     event = "LazyFile",
